5483536f45b4e2decf2e401ff88eecb01b435382
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-19 19:54:29 +0200

    Create custom 404 and 409 exceptions

---
+++ user-service/src/main/java/com/example/userservice/exception/ResourceAlreadyExistException.java
@@ -0,0 +1,12 @@
+package com.example.userservice.exception;
+
+public class ResourceAlreadyExistException extends RuntimeException{
+    public ResourceAlreadyExistException(String msg) {
+        super(msg);
+    }
+
+    public ResourceAlreadyExistException(String resource, String key, Object value) {
+        super(String.format("%s with %s '%s' already exist", resource, key, value));
+    }
+}
+

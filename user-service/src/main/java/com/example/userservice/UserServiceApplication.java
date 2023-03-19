8bb5b6149bb45f32f6df3598bca995c6c2dd6781
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-19 19:23:52 +0200

    Create user service

---
+++ user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.example.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@EnableMongoRepositories
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}

c383e390ffee2953b9e7ea717243b9f4341158db
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-21 13:08:16 +0200

    Create authentication payload

---
+++ subject-service/src/main/java/com/example/subjectservice/payload/auth/TokenExchangeRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.subjectservice.payload.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenExchangeRequestDto {
+    @NotBlank(message = "Token is required")
+    private String token;
+}
---
+++ subject-service/src/main/java/com/example/subjectservice/payload/auth/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.subjectservice.payload.auth;
+
+public enum UserRole {
+    EDUCATOR, STUDENT
+}

692beeba6fabb52c2afef4f77d675fcd50c115db
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-22 18:55:36 +0200

    Create exception handler

---
+++ test-service/src/main/java/com/example/testservice/controller/ExceptionHandlerController.java
@@ -0,0 +1,80 @@
+package com.example.testservice.controller;
+
+import com.example.testservice.exception.ResourceNotFoundException;
+import com.example.testservice.exception.RestTemplateException;
+import com.example.testservice.payload.ApiErrorDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestControllerAdvice
+public class ExceptionHandlerController {
+    private final ObjectMapper objectMapper;
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ApiErrorDto> handleAuthenticationException(BadCredentialsException e) {
+        log.error("Handle bad credentials exception: {}", e.getMessage(), e);
+
+        String error = "Invalid username or password";
+        return buildResponseEntity(new ApiErrorDto(UNAUTHORIZED, error, e));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<ApiErrorDto> handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
+        log.error("Handle message not readable exception: {}", e.getMessage(), e);
+
+        String error = "Malformed JSON request";
+        return buildResponseEntity(new ApiErrorDto(BAD_REQUEST, error, e));
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    protected ResponseEntity<ApiErrorDto> handleResourceNotFound(ResourceNotFoundException e) {
+        log.error("Handle message not readable exception: {}", e.getMessage(), e);
+
+        return buildResponseEntity(new ApiErrorDto(NOT_FOUND, e.getMessage(), e));
+    }
+
+    @ExceptionHandler(BindException.class)
+    protected ResponseEntity<ApiErrorDto> handleMethodArgumentNotValid(BindException e) {
+        log.error("handle bind exception: {}", e.getMessage(), e);
+
+        Optional<FieldError> fieldError = e.getFieldErrors().stream().findFirst();
+        String message = fieldError.isEmpty() ? "Invalid input" : fieldError.get().getDefaultMessage();
+
+        ApiErrorDto apiError = new ApiErrorDto(BAD_REQUEST, message);
+        return buildResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    protected ResponseEntity<ApiErrorDto> handleIllegalStateException(IllegalStateException e) {
+        log.error("handle illegal state exception: {}", e.getMessage(), e);
+
+        return buildResponseEntity(new ApiErrorDto(BAD_REQUEST, e.getMessage(), e));
+    }
+
+    @ExceptionHandler(RestTemplateException.class)
+    public ResponseEntity<ApiErrorDto> handleServerException(RestTemplateException e) throws JsonProcessingException {
+        log.error("Handle rest template exception: {}", e.getMessage(), e);
+
+        ApiErrorDto apiErrorDto = objectMapper.readValue(e.getMessage(), ApiErrorDto.class);
+        return buildResponseEntity(apiErrorDto);
+    }
+
+    private ResponseEntity<ApiErrorDto> buildResponseEntity(ApiErrorDto apiError) {
+        return new ResponseEntity<>(apiError, apiError.getStatus());
+    }
+}

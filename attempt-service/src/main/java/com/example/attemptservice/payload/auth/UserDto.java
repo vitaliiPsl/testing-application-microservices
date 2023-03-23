097bf39acd395ef74ac9f788a9e17738f14b6212
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-23 14:06:56 +0200

    Create authentication payload

---
+++ attempt-service/src/main/java/com/example/attemptservice/payload/auth/UserDto.java
@@ -0,0 +1,29 @@
+package com.example.attemptservice.payload.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+
+    private String id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private UserRole role;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
---
+++ attempt-service/src/main/java/com/example/attemptservice/payload/auth/UserRole.java
@@ -0,0 +1,5 @@
+package com.example.attemptservice.payload.auth;
+
+public enum UserRole {
+    EDUCATOR, STUDENT
+}

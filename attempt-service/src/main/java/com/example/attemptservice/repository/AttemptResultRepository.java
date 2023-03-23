85131481514ffbe27f1cd4259cbdb90f7f0f5106
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-23 14:11:05 +0200

    Create attempt repository

---
+++ attempt-service/src/main/java/com/example/attemptservice/repository/AttemptResultRepository.java
@@ -0,0 +1,15 @@
+package com.example.attemptservice.repository;
+
+import com.example.attemptservice.model.AttemptResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AttemptResultRepository extends JpaRepository<AttemptResult, String> {
+
+    List<AttemptResult> findByUserId(String userId);
+
+    List<AttemptResult> findByTestId(String testId);
+}

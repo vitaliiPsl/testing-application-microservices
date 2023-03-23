7c40aa7e97acaf884e0aa1be0c7c3e13cef7317e
Vitalii Psl <vitalii.psl.main@gmail.com>
2023-03-23 14:05:43 +0200

    Create attempt service

---
+++ attempt-service/src/main/java/com/example/attemptservice/AttemptServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.attemptservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttemptServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttemptServiceApplication.class, args);
+    }
+
+}
